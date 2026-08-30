335b354db6f377c56b9324aaec0e3d96f4718007
ancientpagoda-rgb <ancientpagoda@gmail.com>
2026-08-30 16:19:04 -0500

    Expand Greek and scientific etymology branches

---
+++ etymology-forest/expansion-greek.js
@@ -0,0 +1,128 @@
+__etymAdd(`gr-astron|ἄστρον|Greek|Ancient Greek|star|greek
+gr-nomos|νόμος|Greek|Ancient Greek|law; custom; ordering|greek
+gr-astronomia|ἀστρονομία|Greek|Ancient Greek|astronomy|greek
+astronomy|astronomy|English|Middle English → Modern|study of celestial objects|greek
+gr-ge|γῆ|Greek|Ancient Greek|earth|greek
+gr-graphein|γράφειν|Greek|Ancient Greek|write|greek
+gr-geographia|γεωγραφία|Greek|Ancient Greek|description of the earth|greek
+geography|geography|English|16th c. → Modern|study and description of the earth|greek
+gr-psyche|ψυχή|Greek|Ancient Greek|breath; soul; mind|greek
+nl-psychologia|psychologia|New Latin|16th c.|study of the soul or mind|greek
+psychology|psychology|English|17th c. → Modern|study of mind and behavior|greek
+gr-anthropos|ἄνθρωπος|Greek|Ancient Greek|human being|greek
+nl-anthropologia|anthropologia|New Latin|16th c.|study of humans|greek
+anthropology|anthropology|English|16th c. → Modern|study of humans and cultures|greek
+gr-arkhaios|ἀρχαῖος|Greek|Ancient Greek|ancient|greek
+nl-archaeologia|archaeologia|New Latin|17th c.|study of antiquity|greek
+archaeology|archaeology|English|17th c. → Modern|study of the human past through material remains|greek
+gr-chronos|χρόνος|Greek|Ancient Greek|time|greek
+gr-chronologia|χρονολογία|Greek|Greek|reckoning or study of time|greek
+chronology|chronology|English|16th c. → Modern|arrangement of events in time|greek
+gr-thermos|θερμός|Greek|Ancient Greek|hot|greek
+gr-metron|μέτρον|Greek|Ancient Greek|measure|greek
+thermometer|thermometer|English|17th c. → Modern|instrument for measuring temperature|greek
+gr-phos|φῶς / φωτός|Greek|Ancient Greek|light|greek
+gr-graphe|γραφή|Greek|Ancient Greek|writing; drawing|greek
+photograph|photograph|English|19th c.|image made by recording light|greek
+gr-tele|τῆλε|Greek|Ancient Greek|far off|greek
+gr-phone|φωνή|Greek|Ancient Greek|voice; sound|greek
+telephone|telephone|English|19th c.|device for transmitting voice over distance|greek
+gr-neuron|νεῦρον|Greek|Ancient Greek|sinew; nerve|greek
+nl-neurologia|neurologia|New Latin|modern scientific coinage|study of nerves|greek
+neurology|neurology|English|17th–19th c.|study of the nervous system|greek
+gr-pathos|πάθος|Greek|Ancient Greek|suffering; feeling; experience|greek
+nl-pathologia|pathologia|New Latin|modern scientific formation|study of disease|greek
+pathology|pathology|English|17th c. → Modern|study of disease|greek
+gr-oikos|οἶκος|Greek|Ancient Greek|house; household|greek
+de-oekologie|Ökologie|German|19th c.|ecology|greek
+ecology|ecology|English|19th c. → Modern|study of organisms and environment|greek
+gr-techne|τέχνη|Greek|Ancient Greek|art; craft; skill|greek
+nl-technologia|technologia|New Latin|learned formation|systematic treatment of an art|greek
+technology|technology|English|17th c. → Modern|application and study of technical knowledge|greek
+gr-autos|αὐτός|Greek|Ancient Greek|self|greek
+gr-autonomia|αὐτονομία|Greek|Ancient Greek|self-government|greek
+autonomy|autonomy|English|17th c. → Modern|self-government; independence|greek
+gr-monos|μόνος|Greek|Ancient Greek|alone; single|greek
+gr-arkhe|ἀρχή|Greek|Ancient Greek|rule; beginning|greek
+gr-monarkhia|μοναρχία|Greek|Ancient Greek|rule by one|greek
+lat-monarchia|monarchia|Latin|Late Latin|monarchy|romance
+of-monarchie|monarchie|Old French|Old French|monarchy|romance
+monarchy|monarchy|English|Middle English → Modern|government by one monarch|greek
+gr-aristos|ἄριστος|Greek|Ancient Greek|best|greek
+gr-aristokratia|ἀριστοκρατία|Greek|Ancient Greek|rule of the best|greek
+aristocracy|aristocracy|English|16th c. → Modern|rule by a privileged class|greek
+gr-pherein|φέρειν|Greek|Ancient Greek|carry; bear|greek
+gr-meta|μετά|Greek|Ancient Greek|after; across; beyond|greek
+gr-metaphora|μεταφορά|Greek|Ancient Greek|transfer|greek
+lat-metaphora|metaphora|Latin|Latin|metaphor|romance
+metaphor|metaphor|English|16th c. → Modern|figurative transfer of a word or idea|greek
+gr-kardia|καρδία|Greek|Ancient Greek|heart|greek
+gr-kardiakos|καρδιακός|Greek|Ancient Greek|of the heart|greek
+cardiac|cardiac|English|17th c. → Modern|of the heart|greek
+gr-genesis|γένεσις|Greek|Ancient Greek|origin; birth|greek
+lat-genesis|genesis|Latin|Latin|origin; creation|romance
+genesis|genesis|English|Old English/learned reborrowings|origin; beginning|greek
+gr-gnosis|γνῶσις|Greek|Ancient Greek|knowledge|greek
+gr-diagnosis|διάγνωσις|Greek|Ancient Greek|discernment; distinguishing|greek
+diagnosis|diagnosis|English|17th c. → Modern|identification of a disease or condition|greek`,`pie-star>gr-astron:inherited
+gr-astron>gr-astronomia:compound
+gr-nomos>gr-astronomia:compound
+gr-astronomia>astronomy:learned
+gr-ge>gr-geographia:compound
+gr-graphein>gr-geographia:compound
+gr-geographia>geography:learned
+gr-psyche>nl-psychologia:compound
+gr-logos>nl-psychologia:compound
+nl-psychologia>psychology:borrowed
+gr-anthropos>nl-anthropologia:compound
+gr-logos>nl-anthropologia:compound
+nl-anthropologia>anthropology:borrowed
+gr-arkhaios>nl-archaeologia:compound
+gr-logos>nl-archaeologia:compound
+nl-archaeologia>archaeology:borrowed
+gr-chronos>gr-chronologia:compound
+gr-logos>gr-chronologia:compound
+gr-chronologia>chronology:learned
+gr-thermos>thermometer:compound
+gr-metron>thermometer:compound
+gr-phos>photograph:compound
+gr-graphe>photograph:compound
+gr-tele>telephone:compound
+gr-phone>telephone:compound
+gr-neuron>nl-neurologia:compound
+gr-logos>nl-neurologia:compound
+nl-neurologia>neurology:borrowed
+gr-pathos>nl-pathologia:compound
+gr-logos>nl-pathologia:compound
+nl-pathologia>pathology:borrowed
+gr-oikos>de-oekologie:compound
+gr-logos>de-oekologie:compound
+de-oekologie>ecology:borrowed
+gr-techne>nl-technologia:compound
+gr-logos>nl-technologia:compound
+nl-technologia>technology:borrowed
+gr-autos>gr-autonomia:compound
+gr-nomos>gr-autonomia:compound
+gr-autonomia>autonomy:learned
+gr-monos>gr-monarkhia:compound
+gr-arkhe>gr-monarkhia:compound
+gr-monarkhia>lat-monarchia:borrowed
+lat-monarchia>of-monarchie:borrowed
+of-monarchie>monarchy:borrowed
+gr-aristos>gr-aristokratia:compound
+gr-kratos>gr-aristokratia:compound
+gr-aristokratia>aristocracy:learned
+pie-bear>gr-pherein:inherited
+gr-meta>gr-metaphora:compound
+gr-pherein>gr-metaphora:derived
+gr-metaphora>lat-metaphora:borrowed
+lat-metaphora>metaphor:borrowed
+pie-heart>gr-kardia:inherited
+gr-kardia>gr-kardiakos:derived
+gr-kardiakos>cardiac:learned
+pie-gen>gr-genesis:inherited
+gr-genesis>lat-genesis:borrowed
+lat-genesis>genesis:borrowed
+pie-know>gr-gnosis:inherited
+gr-gnosis>gr-diagnosis:derived
+gr-diagnosis>diagnosis:learned`,{"German":"#94b5a3"});
