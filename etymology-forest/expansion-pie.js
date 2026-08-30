90177de73139ca54d2593a295a8e4c6c04276d67
ancientpagoda-rgb <ancientpagoda@gmail.com>
2026-08-30 16:18:38 -0500

    Expand Proto-Indo-European etymology branches

---
+++ etymology-forest/expansion-pie.js
@@ -0,0 +1,133 @@
+__etymAdd(`pie-daughter|*dʰugh₂tḗr|Proto-Indo-European|reconstructed|daughter|pie
+pg-daughter|*duhtēr|Proto-Germanic|reconstructed|daughter|germanic
+oe-daughter|dohtor|Old English|Old English|daughter|germanic
+daughter|daughter|English|Modern|female child|germanic
+pie-son|*suHnús|Proto-Indo-European|reconstructed|son|pie
+pg-son|*sunuz|Proto-Germanic|reconstructed|son|germanic
+oe-son|sunu|Old English|Old English|son|germanic
+son|son|English|Modern|male child|germanic
+pie-water|*wódr̥|Proto-Indo-European|reconstructed|water|pie
+pg-water|*watōr|Proto-Germanic|reconstructed|water|germanic
+oe-water|wæter|Old English|Old English|water|germanic
+water|water|English|Modern|water|germanic
+pie-star|*h₂stḗr|Proto-Indo-European|reconstructed|star|pie
+pg-star|*sternô|Proto-Germanic|reconstructed|star|germanic
+oe-star|steorra|Old English|Old English|star|germanic
+star|star|English|Modern|star|germanic
+pie-sun|*sóh₂wl̥|Proto-Indo-European|reconstructed|sun|pie
+pg-sun|*sunnǭ|Proto-Germanic|reconstructed|sun|germanic
+oe-sun|sunne|Old English|Old English|sun|germanic
+sun|sun|English|Modern|the Sun|germanic
+pie-moon|*mḗh₁n̥s|Proto-Indo-European|reconstructed|moon; month|pie
+pg-moon|*mēnô|Proto-Germanic|reconstructed|moon|germanic
+oe-moon|mōna|Old English|Old English|moon|germanic
+moon|moon|English|Modern|natural satellite; moon|germanic
+pie-tooth|*h₃dónts|Proto-Indo-European|reconstructed|tooth|pie
+pg-tooth|*tanþs|Proto-Germanic|reconstructed|tooth|germanic
+oe-tooth|tōþ|Old English|Old English|tooth|germanic
+tooth|tooth|English|Modern|tooth|germanic
+pie-foot|*pṓds|Proto-Indo-European|reconstructed|foot|pie
+pg-foot|*fōts|Proto-Germanic|reconstructed|foot|germanic
+oe-foot|fōt|Old English|Old English|foot|germanic
+foot|foot|English|Modern|foot|germanic
+pie-eat|*h₁ed-|Proto-Indo-European|reconstructed|eat|pie
+pg-eat|*etaną|Proto-Germanic|reconstructed|eat|germanic
+oe-eat|etan|Old English|Old English|eat|germanic
+eat|eat|English|Modern|consume food|germanic
+pie-sit|*sed-|Proto-Indo-European|reconstructed|sit|pie
+pg-sit|*sitjaną|Proto-Germanic|reconstructed|sit|germanic
+oe-sit|sittan|Old English|Old English|sit|germanic
+sit|sit|English|Modern|be seated|germanic
+pie-stand|*steh₂-|Proto-Indo-European|reconstructed|stand|pie
+pg-stand|*standaną|Proto-Germanic|reconstructed|stand|germanic
+oe-stand|standan|Old English|Old English|stand|germanic
+stand|stand|English|Modern|be upright|germanic
+pie-bear|*bʰer-|Proto-Indo-European|reconstructed|carry; bear|pie
+pg-bear|*beraną|Proto-Germanic|reconstructed|carry; bear|germanic
+oe-bear|beran|Old English|Old English|bear; carry|germanic
+bear|bear|English|Modern|carry; endure; give birth|germanic
+pie-know|*ǵneh₃-|Proto-Indo-European|reconstructed|know|pie
+pg-know|*knēaną|Proto-Germanic|reconstructed|know|germanic
+oe-know|cnāwan|Old English|Old English|know|germanic
+know|know|English|Modern|be aware of; understand|germanic
+pie-new|*néwos|Proto-Indo-European|reconstructed|new|pie
+pg-new|*newjaz|Proto-Germanic|reconstructed|new|germanic
+oe-new|nīwe|Old English|Old English|new|germanic
+new|new|English|Modern|new|germanic
+pie-red|*h₁rewdʰ-|Proto-Indo-European|reconstructed|red|pie
+pg-red|*raudaz|Proto-Germanic|reconstructed|red|germanic
+oe-red|rēad|Old English|Old English|red|germanic
+red|red|English|Modern|red|germanic
+pie-three|*tréyes|Proto-Indo-European|reconstructed|three|pie
+pg-three|*þrīz|Proto-Germanic|reconstructed|three|germanic
+oe-three|þrīe|Old English|Old English|three|germanic
+three|three|English|Modern|number 3|germanic
+pie-seven|*septḿ̥|Proto-Indo-European|reconstructed|seven|pie
+pg-seven|*sebun|Proto-Germanic|reconstructed|seven|germanic
+oe-seven|seofon|Old English|Old English|seven|germanic
+seven|seven|English|Modern|number 7|germanic
+pie-ten|*déḱm̥|Proto-Indo-European|reconstructed|ten|pie
+pg-ten|*tehun|Proto-Germanic|reconstructed|ten|germanic
+oe-ten|tīen|Old English|Old English|ten|germanic
+ten|ten|English|Modern|number 10|germanic
+pie-heart|*ḱerd-|Proto-Indo-European|reconstructed stem|heart|pie
+pg-heart|*hertô|Proto-Germanic|reconstructed|heart|germanic
+oe-heart|heorte|Old English|Old English|heart|germanic
+heart|heart|English|Modern|heart|germanic
+pie-gen|*ǵenh₁-|Proto-Indo-European|reconstructed|beget; give birth|pie`,`pie-daughter>pg-daughter:inherited
+pg-daughter>oe-daughter:inherited
+oe-daughter>daughter:inherited
+pie-son>pg-son:inherited
+pg-son>oe-son:inherited
+oe-son>son:inherited
+pie-water>pg-water:inherited
+pg-water>oe-water:inherited
+oe-water>water:inherited
+pie-star>pg-star:inherited
+pg-star>oe-star:inherited
+oe-star>star:inherited
+pie-sun>pg-sun:inherited
+pg-sun>oe-sun:inherited
+oe-sun>sun:inherited
+pie-moon>pg-moon:inherited
+pg-moon>oe-moon:inherited
+oe-moon>moon:inherited
+pie-tooth>pg-tooth:inherited
+pg-tooth>oe-tooth:inherited
+oe-tooth>tooth:inherited
+pie-foot>pg-foot:inherited
+pg-foot>oe-foot:inherited
+oe-foot>foot:inherited
+pie-eat>pg-eat:inherited
+pg-eat>oe-eat:inherited
+oe-eat>eat:inherited
+pie-sit>pg-sit:inherited
+pg-sit>oe-sit:inherited
+oe-sit>sit:inherited
+pie-stand>pg-stand:inherited
+pg-stand>oe-stand:inherited
+oe-stand>stand:inherited
+pie-bear>pg-bear:inherited
+pg-bear>oe-bear:inherited
+oe-bear>bear:inherited
+pie-know>pg-know:inherited
+pg-know>oe-know:inherited
+oe-know>know:inherited
+pie-new>pg-new:inherited
+pg-new>oe-new:inherited
+oe-new>new:inherited
+pie-red>pg-red:inherited
+pg-red>oe-red:inherited
+oe-red>red:inherited
+pie-three>pg-three:inherited
+pg-three>oe-three:inherited
+oe-three>three:inherited
+pie-seven>pg-seven:inherited
+pg-seven>oe-seven:inherited
+oe-seven>seven:inherited
+pie-ten>pg-ten:inherited
+pg-ten>oe-ten:inherited
+oe-ten>ten:inherited
+pie-heart>pg-heart:inherited
+pg-heart>oe-heart:inherited
+oe-heart>heart:inherited`,{});
