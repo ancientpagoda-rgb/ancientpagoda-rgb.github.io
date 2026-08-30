b14617153e7defcdfa5070c5058376219c40647d
ancientpagoda-rgb <ancientpagoda@gmail.com>
2026-08-30 16:19:41 -0500

    Expand borrowed-word etymology routes

---
+++ etymology-forest/expansion-borrow.js
@@ -0,0 +1,206 @@
+__etymAdd(`ar-khwarizmi|al-Khwārizmī|Arabic|Medieval Arabic|name of the mathematician al-Khwarizmi|borrow
+ml-algorismus|algorismus|Latin|Medieval Latin|arithmetic by Hindu-Arabic numerals|borrow
+algorithm|algorithm|English|Middle English → Modern|stepwise computational procedure|borrow
+ar-sifr|ṣifr|Arabic|Arabic|empty; zero|borrow
+ml-cifra|cifra|Latin|Medieval Latin|zero; numeral|borrow
+of-cifre|cifre|Old French|Old French|cipher|romance
+cipher|cipher|English|Middle English → Modern|code; zero historically|borrow
+it-zefiro|zefiro|Italian|Italian|zero|borrow
+zero|zero|English|17th c. → Modern|number 0|borrow
+ar-sumut|as-sumūt|Arabic|Medieval Arabic|the directions|borrow
+of-azimut|azimut|Old French|Old French|azimuth|romance
+azimuth|azimuth|English|14th c. → Modern|horizontal angular direction|borrow
+ar-samt|samt|Arabic|Medieval Arabic|direction; path|borrow
+ml-cenit|cenit|Latin|Medieval Latin|zenith|borrow
+zenith|zenith|English|14th c. → Modern|point directly overhead|borrow
+ar-nazir|naẓīr|Arabic|Medieval Arabic|counterpart; opposite|borrow
+ml-nadir|nadir|Latin|Medieval Latin|nadir|borrow
+nadir|nadir|English|15th c. → Modern|point directly below; lowest point|borrow
+ar-matrah|maṭraḥ|Arabic|Arabic|place where something is thrown; cushion|borrow
+of-materas|materas|Old French|Old French|mattress|romance
+mattress|mattress|English|Middle English → Modern|bed cushion|borrow
+ar-tarifa|taʿrīfa|Arabic|Arabic|notification; price list|borrow
+it-tariffa|tariffa|Italian|Italian|schedule of prices or duties|borrow
+tariff|tariff|English|16th c. → Modern|schedule of customs duties|borrow
+ar-makhazin|makhāzin|Arabic|Arabic|storehouses|borrow
+it-magazzino|magazzino|Italian|Italian|storehouse|borrow
+fr-magasin|magasin|French|French|store; warehouse|borrow
+magazine|magazine|English|16th c. → Modern|storehouse; later periodical publication|borrow
+ar-safar|safar|Arabic|Arabic|journey|borrow
+sw-safari|safari|Swahili|Swahili|journey; expedition|borrow
+safari|safari|English|19th c. → Modern|expedition, especially to observe wildlife|borrow
+fa-bazar|bāzār|Persian|Persian|market|borrow
+bazaar|bazaar|English|16th c. → Modern|marketplace|borrow
+fa-karvan|kārvān|Persian|Persian|group of travelers|borrow
+fr-caravane|caravane|French|French|caravan|borrow
+caravan|caravan|English|16th c. → Modern|group traveling together|borrow
+oir-pairidaiza|*pari-daiza|Old Iranian|reconstructed/attested Iranian formation|walled enclosure|borrow
+gr-paradeisos|παράδεισος|Greek|Ancient Greek|park; enclosed garden|greek
+lat-paradisus|paradisus|Latin|Late Latin|paradise|romance
+of-paradis|paradis|Old French|Old French|paradise|romance
+paradise|paradise|English|Middle English → Modern|heaven; ideal place|borrow
+sa-jangala|jaṅgala|Sanskrit|Sanskrit|dry; rough terrain|borrow
+hi-jangal|jaṅgal|Hindi|Hindi|wilderness; forest|borrow
+jungle|jungle|English|18th c. → Modern|dense tropical vegetation; wilderness|borrow
+fa-khak|khāk|Persian|Persian|dust; soil|borrow
+ur-khaki|khākī|Urdu|Urdu|dust-colored|borrow
+khaki|khaki|English|19th c. → Modern|dust-colored cloth or color|borrow
+fa-payjama|pāy-jāma|Persian|Persian|leg garment|borrow
+hi-pajama|pāyjāma|Hindi|Hindi/Urdu|loose trousers|borrow
+pajamas|pajamas|English|19th c. → Modern|sleepwear; originally loose trousers|borrow
+hi-loot|lūṭ|Hindi|Hindi|plunder|borrow
+loot|loot|English|19th c. → Modern|stolen goods; plunder|borrow
+hi-thag|ṭhag|Hindi|Hindi|swindler; robber|borrow
+thug|thug|English|19th c. → Modern|violent criminal or intimidating person|borrow
+sa-pandita|paṇḍita|Sanskrit|Sanskrit|learned; scholar|borrow
+hi-pandit|paṇḍit|Hindi|Hindi|learned person|borrow
+pundit|pundit|English|17th c. → Modern|expert commentator; learned person|borrow
+sa-guru|guru|Sanskrit|Sanskrit|teacher; venerable person|borrow
+guru|guru|English|20th c. → Modern|teacher or influential expert|borrow
+sa-yoga|yoga|Sanskrit|Sanskrit|yoking; union; discipline|borrow
+yoga|yoga|English|19th c. → Modern|Indian physical, mental, and spiritual discipline|borrow
+sa-karman|karman|Sanskrit|Sanskrit|act; deed|borrow
+karma|karma|English|19th c. → Modern|action and its consequences|borrow
+sa-nirvana|nirvāṇa|Sanskrit|Sanskrit|extinguishing; liberation|borrow
+nirvana|nirvana|English|19th c. → Modern|liberation; figuratively bliss|borrow
+jp-tsunami|津波|Japanese|Japanese|harbor wave|borrow
+tsunami|tsunami|English|19th–20th c.|large sea wave caused by displacement|borrow
+jp-karaoke|カラオケ|Japanese|Japanese|empty orchestra|borrow
+karaoke|karaoke|English|20th c.|singing to prerecorded accompaniment|borrow
+jp-emoji|絵文字|Japanese|Japanese|picture character|borrow
+emoji|emoji|English|20th–21st c.|small digital pictograph|borrow
+jp-samurai|侍|Japanese|Japanese|warrior retainer|borrow
+samurai|samurai|English|18th–19th c.|member of the Japanese warrior class|borrow
+jp-ninja|忍者|Japanese|Japanese|person trained in stealth arts|borrow
+ninja|ninja|English|20th c.|Japanese covert warrior; figuratively an expert|borrow
+jp-taikun|大君|Japanese|Japanese|great lord|borrow
+tycoon|tycoon|English|19th c. → Modern|powerful business person|borrow
+jp-haiku|俳句|Japanese|Japanese|haiku verse|borrow
+haiku|haiku|English|19th–20th c.|Japanese three-line poetic form|borrow
+nah-tomatl|tomatl|Nahuatl|Nahuatl|tomato|borrow
+es-tomate|tomate|Spanish|Spanish|tomato|borrow
+tomato|tomato|English|17th c. → Modern|tomato|borrow
+nah-ahuacatl|āhuacatl|Nahuatl|Nahuatl|avocado|borrow
+es-aguacate|aguacate|Spanish|Spanish|avocado|borrow
+avocado|avocado|English|17th c. → Modern|avocado fruit|borrow
+nah-coyotl|coyōtl|Nahuatl|Nahuatl|coyote|borrow
+es-coyote|coyote|Spanish|Spanish|coyote|borrow
+coyote|coyote|English|19th c. → Modern|North American canid|borrow
+nah-chilli|chīlli|Nahuatl|Nahuatl|chili pepper|borrow
+es-chile|chile|Spanish|Spanish|chili pepper|borrow
+chili|chili|English|17th c. → Modern|chili pepper|borrow
+tai-hamaca|hamaca|Taíno|Taíno|hammock|borrow
+es-hamaca|hamaca|Spanish|Spanish|hammock|borrow
+hammock|hammock|English|16th c. → Modern|suspended bed or sling|borrow
+tai-canoa|canoa|Taíno|Taíno|dugout boat|borrow
+es-canoa|canoa|Spanish|Spanish|canoe|borrow
+canoe|canoe|English|16th c. → Modern|narrow paddled boat|borrow
+tai-hurakan|hurakán|Taíno|Taíno|storm deity / hurricane|borrow
+es-huracan|huracán|Spanish|Spanish|hurricane|borrow
+hurricane|hurricane|English|16th c. → Modern|tropical cyclone|borrow
+ms-amuk|amuk|Malay|Malay|attack furiously|borrow
+amok|amok|English|17th c. → Modern|in a violently uncontrolled manner|borrow
+ms-orang|orang hutan|Malay|Malay|forest person|borrow
+orangutan|orangutan|English|17th c. → Modern|great ape of Borneo and Sumatra|borrow
+ms-bambu|bambu|Malay|Malay|bamboo|borrow
+nl-bamboe|bamboe|Dutch|Dutch|bamboo|borrow
+bamboo|bamboo|English|17th c. → Modern|woody grass|borrow
+nan-te|tê|Hokkien|Southern Min/Hokkien|tea|borrow
+nl-thee|thee|Dutch|Dutch|tea|borrow
+tea|tea|English|17th c. → Modern|tea beverage or plant|borrow
+tr-yogurt|yoğurt|Turkish|Turkish|yogurt|borrow
+yogurt|yogurt|English|17th c. → Modern|fermented milk food|borrow
+fa-kushk|kūšk|Persian|Persian|pavilion; palace|borrow
+tr-kosk|köşk|Turkish|Turkish|pavilion|borrow
+fr-kiosque|kiosque|French|French|kiosk|borrow
+kiosk|kiosk|English|17th c. → Modern|small open-fronted booth|borrow
+mi-kiwi|kiwi|Māori|Māori|kiwi bird|borrow
+kiwi|kiwi|English|19th c. → Modern|New Zealand bird; later other senses|borrow
+gug-gangurru|gangurru|Guugu Yimidhirr|Guugu Yimidhirr|large black or grey kangaroo|borrow
+kangaroo|kangaroo|English|18th c. → Modern|Australian marsupial|borrow
+yi-beygl|בײגל / beygl|Yiddish|Yiddish|ring-shaped bread roll|borrow
+bagel|bagel|English|20th c. → Modern|ring-shaped bread roll|borrow
+he-shabbat|שַׁבָּת / shabbāt|Hebrew|Hebrew|rest; Sabbath|borrow
+gr-sabbaton|σάββατον|Greek|Koine Greek|Sabbath|greek
+lat-sabbatum|sabbatum|Latin|Late Latin|Sabbath|romance
+sabbath|Sabbath|English|Old English → Modern|weekly day of rest or worship|borrow`,`ar-khwarizmi>ml-algorismus:borrowed
+ml-algorismus>algorithm:borrowed
+ar-sifr>ml-cifra:borrowed
+ml-cifra>of-cifre:borrowed
+of-cifre>cipher:borrowed
+ar-sifr>it-zefiro:borrowed
+it-zefiro>zero:borrowed
+ar-sumut>of-azimut:borrowed
+of-azimut>azimuth:borrowed
+ar-samt>ml-cenit:borrowed
+ml-cenit>zenith:borrowed
+ar-nazir>ml-nadir:borrowed
+ml-nadir>nadir:borrowed
+ar-matrah>of-materas:borrowed
+of-materas>mattress:borrowed
+ar-tarifa>it-tariffa:borrowed
+it-tariffa>tariff:borrowed
+ar-makhazin>it-magazzino:borrowed
+it-magazzino>fr-magasin:borrowed
+fr-magasin>magazine:borrowed
+ar-safar>sw-safari:borrowed
+sw-safari>safari:borrowed
+fa-bazar>bazaar:borrowed
+fa-karvan>fr-caravane:borrowed
+fr-caravane>caravan:borrowed
+oir-pairidaiza>gr-paradeisos:borrowed
+gr-paradeisos>lat-paradisus:borrowed
+lat-paradisus>of-paradis:borrowed
+of-paradis>paradise:borrowed
+sa-jangala>hi-jangal:borrowed
+hi-jangal>jungle:borrowed
+fa-khak>ur-khaki:derived
+ur-khaki>khaki:borrowed
+fa-payjama>hi-pajama:borrowed
+hi-pajama>pajamas:borrowed
+hi-loot>loot:borrowed
+hi-thag>thug:borrowed
+sa-pandita>hi-pandit:borrowed
+hi-pandit>pundit:borrowed
+sa-guru>guru:borrowed
+sa-yoga>yoga:borrowed
+sa-karman>karma:borrowed
+sa-nirvana>nirvana:borrowed
+jp-tsunami>tsunami:borrowed
+jp-karaoke>karaoke:borrowed
+jp-emoji>emoji:borrowed
+jp-samurai>samurai:borrowed
+jp-ninja>ninja:borrowed
+jp-taikun>tycoon:borrowed
+jp-haiku>haiku:borrowed
+nah-tomatl>es-tomate:borrowed
+es-tomate>tomato:borrowed
+nah-ahuacatl>es-aguacate:borrowed
+es-aguacate>avocado:borrowed
+nah-coyotl>es-coyote:borrowed
+es-coyote>coyote:borrowed
+nah-chilli>es-chile:borrowed
+es-chile>chili:borrowed
+tai-hamaca>es-hamaca:borrowed
+es-hamaca>hammock:borrowed
+tai-canoa>es-canoa:borrowed
+es-canoa>canoe:borrowed
+tai-hurakan>es-huracan:borrowed
+es-huracan>hurricane:borrowed
+ms-amuk>amok:borrowed
+ms-orang>orangutan:borrowed
+ms-bambu>nl-bamboe:borrowed
+nl-bamboe>bamboo:borrowed
+nan-te>nl-thee:borrowed
+nl-thee>tea:borrowed
+tr-yogurt>yogurt:borrowed
+fa-kushk>tr-kosk:borrowed
+tr-kosk>fr-kiosque:borrowed
+fr-kiosque>kiosk:borrowed
+mi-kiwi>kiwi:borrowed
+gug-gangurru>kangaroo:borrowed
+yi-beygl>bagel:borrowed
+he-shabbat>gr-sabbaton:borrowed
+gr-sabbaton>lat-sabbatum:borrowed
+lat-sabbatum>sabbath:borrowed`,{"French":"#d08b80","Old Iranian":"#c1a36c","Turkish":"#bca873","Spanish":"#d78f76","Portuguese":"#ce987c","Urdu":"#d49b78","Nahuatl":"#d6a86d","Taíno":"#cfa275","Malay":"#d79c84","Hokkien":"#d5a18f","Dutch":"#8eae9f","Swahili":"#d9aa76","Māori":"#c69fa7","Guugu Yimidhirr":"#c2a48d","Yiddish":"#b6a4cc","Hebrew":"#c5a6c6"});
+__etymFinalize();
